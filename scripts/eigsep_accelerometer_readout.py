e607e9a5be1d458526e43be191d32afbed6581a9
pronav5000 <pranavkarthik@berkeley.edu>
2025-04-18 12:45:07 -0700

    modified to call accelerometer class

--- scripts/eigsep_accelerometer_readout.py
+++ scripts/eigsep_accelerometer_readout.py
@@ -4,18 +4,16 @@ import numpy as np
 #import os
 import board
 import adafruit_mma8451
+import eigsep_sensors as eig
 
-from eigsep_sensors.accelerometer import calculate_orientation, transform_data_based_on_orientation
-
-i2c = board.I2C()
-sensor = adafruit_mma8451.MMA8451(i2c)
+accelerometer = eig.Accelerometer()
 
 stored_data = []
 while True:
-    x, y, z = sensor.acceleration
-    theta, phi = calculate_orientation(x, y, z)
+    x, y, z = accelerometer.sensor.acceleration
+    theta, phi = eig.calculate_orientation(x, y, z)
     measured_data = 100
-    transformed_data = transform_data_based_on_orientation(measured_data, theta)
+    transformed_data = eig.transform_data_based_on_orientation(measured_data, theta)
     data_entry = {
         "time": time.time(),
         "x": x,
