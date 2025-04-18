0f8b32bd54a8b46bfb7f6f379b0be938a1b3a00b
EIGSEP Motor Pi <chb@berkeley.edu>
2025-04-18 13:04:59 -0700

    updated with error handling

--- scripts/eigsep_accelerometer_readout.py
+++ scripts/eigsep_accelerometer_readout.py
@@ -10,27 +10,31 @@ accelerometer = eig.Accelerometer()
 
 stored_data = []
 while True:
-    x, y, z = accelerometer.sensor.acceleration
-    theta, phi = eig.calculate_orientation(x, y, z)
-    measured_data = 100
-    transformed_data = eig.transform_data_based_on_orientation(measured_data, theta)
-    data_entry = {
-        "time": time.time(),
-        "x": x,
-        "y": y,
-        "z": z,
-        "theta_deg": theta,
-        "phi_deg": phi,
-        "transformed_data": transformed_data
-    }
-    stored_data.append(data_entry)
-    print(f"Time: {data_entry['time']:.2f}")
-    print(f"Accelerometer X: {x:.2f} Y: {y:.2f} Z: {z:.2f}")
-    print(f"Azimuthal Angle (θ): {theta:.2f}°")
-    print(f"Polar Angle (φ): {phi:.2f}°")
-    print(f"Transformed Data: {transformed_data:.2f}")
-    print("-" * 50)
-    time.sleep(0.25)
+    try:
+        x, y, z = accelerometer.sensor.acceleration
+        theta, phi = eig.calculate_orientation(x, y, z)
+        measured_data = 100
+        transformed_data = eig.transform_data_based_on_orientation(measured_data, theta)
+        data_entry = {
+            "time": time.time(),
+            "x": x,
+            "y": y,
+            "z": z,
+            "theta_deg": theta,
+            "phi_deg": phi,
+            "transformed_data": transformed_data
+        }
+        stored_data.append(data_entry)
+        print(f"Time: {data_entry['time']:.2f}")
+        print(f"Accelerometer X: {x:.2f} Y: {y:.2f} Z: {z:.2f}")
+        print(f"Azimuthal Angle (θ): {theta:.2f}°")
+        print(f"Polar Angle (φ): {phi:.2f}°")
+        print(f"Transformed Data: {transformed_data:.2f}")
+        print("-" * 50)
+        time.sleep(0.25)
+    except Exception as e:
+        print(e)
+        time.sleep(0.25)
 
 #if not os.path.exists('data'):
 #    os.makedirs('data')
