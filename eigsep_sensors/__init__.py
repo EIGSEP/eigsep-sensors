fe97596564fb7b2ea3e958e9945890ffb371b5cb
pronav5000 <pranavkarthik@berkeley.edu>
2025-04-18 12:44:54 -0700

    modified to reflect new structure

--- eigsep_sensors/__init__.py
+++ eigsep_sensors/__init__.py
@@ -1,1 +1,5 @@
-from . import accelerometer
+__author__ = "EIGSEP Team"
+__version__ = "0.0.1"
+
+from .accel_orientation import calculate_orientation, transform_data_based_on_orientation
+from .accelerometer import Accelerometer
