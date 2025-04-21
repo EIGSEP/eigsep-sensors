8bf618b24b8b2c5b968d93f1846da6f40e0f3239
pronav5000 <pranavkarthik@berkeley.edu>
2025-04-21 15:03:04 -0700

    updated with new functions

--- eigsep_sensors/__init__.py
+++ eigsep_sensors/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "EIGSEP Team"
 __version__ = "0.0.1"
 
-from .accel_orientation import calculate_orientation, transform_data_based_on_orientation
+from .accel_orientation import calculate_orientation, get_orientation_unit_vector, get_pitch_roll_from_unit_vector, angle_with_vertical
 from .accelerometer import Accelerometer
