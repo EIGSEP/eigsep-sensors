7d85f5331a24d9bf0d73076e00c2466b49158ea2
Christian Hellum Bye <chbye@berkeley.edu>
2025-06-07 14:54:14 -0700

    finish thermistor tests

--- tests/test_thermistor.py
+++ tests/test_thermistor.py
@@ -24,10 +24,10 @@ def test_steinhart_hart(logR):
     if logR > 0:
         coeffs = {"B": 1}
         temp = th.steinhart_hart(r_therm, coeffs)
-        assert temp == 1 / logR
+        assert temp == pytest.approx(1 / logR)
         coeffs = {"C": 1}
         temp = th.steinhart_hart(r_therm, coeffs)
-        assert temp == (1 / logR) ** 3
+        assert temp == pytest.approx((1 / logR) ** 3)
     else:
         coeffs = {"B": 1}
         with pytest.raises(ValueError):  # t_inv cannot be zero
@@ -59,15 +59,23 @@ def test_raw_to_temp(therm):
     raw = therm.max_adc_value // 2
     temp = therm.raw_to_temp(raw)
     expected_rtherm = therm.R_fixed  # same resistance for both resistors
+    log_rtherm = np.log(expected_rtherm)
     expected_temp = th.steinhart_hart(expected_rtherm, therm.sh_coeff)
-    assert temp == expected_temp
+    expected_celsius = expected_temp - 273.15
+    assert temp == pytest.approx(expected_celsius, abs=1e-2)
     # the choice of coefficients matter
-    therm.set_temp_constants({"A": 1})
-    assert therm.raw_to_temp(raw) == 1  # only A is non-zero
-    therm.set_temp_constants({"B": 1})
-    assert therm.raw_to_temp(raw) == 1 / np.log(expected_rtherm)
-    therm.set_temp_constants({"C": 1})
-    assert therm.raw_to_temp(raw) == 1 / np.log(expected_rtherm) ** 3
+    therm._set_temp_constants({"A": 1})
+    expected_temp = 1  # only A is non-zero
+    expected_celsius = expected_temp - 273.15
+    assert therm.raw_to_temp(raw) == pytest.approx(expected_celsius, abs=1e-2)
+    therm._set_temp_constants({"B": 1})
+    expected_temp = 1 / log_rtherm
+    expected_celsius = expected_temp - 273.15
+    assert therm.raw_to_temp(raw) == pytest.approx(expected_celsius, abs=1e-2)
+    therm._set_temp_constants({"C": 1})
+    expected_temp = (1 / log_rtherm) ** 3
+    expected_celsius = expected_temp - 273.15
+    assert therm.raw_to_temp(raw) == pytest.approx(expected_celsius, abs=1e-2)
 
 
 def test_read_temperature(therm):
@@ -81,4 +89,5 @@ def test_read_temperature(therm):
     # calculate expected temperature like in test_raw_to_temp
     expected_rtherm = therm.R_fixed
     expected_temp = th.steinhart_hart(expected_rtherm, therm.sh_coeff)
-    assert out == expected_temp
+    expected_celsius = expected_temp - 273.15
+    assert out == pytest.approx(expected_celsius, abs=1e-2)
