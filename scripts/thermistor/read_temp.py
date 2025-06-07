4c4d2935e875c4726a89bcfb4f00db47e70c79d0
Christian Hellum Bye <chbye@berkeley.edu>
2025-06-07 16:06:54 -0700

    add logging, fix bug where counter not incremented

--- scripts/thermistor/read_temp.py
+++ scripts/thermistor/read_temp.py
@@ -6,17 +6,27 @@ Pico.
 """
 
 from datetime import datetime
-import time
+import logging
 import numpy as np
+import time
 
 from eigsep_sensors import Thermistor
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+logger.debug("Initializing thermistor class")
 PORT = "/dev/ttyACM0"
 TIMEOUT = 1
-N_READINGS = 100  # readings per file
-SLEEP_TIME = 60  # seconds between readings
+N_READINGS = 10  # readings per file
+SLEEP_TIME = 1  # seconds between readings
 all_readings = np.zeros(N_READINGS)
 therm = Thermistor(PORT, timeout=TIMEOUT)
+
+logger.info("Thermistor initialized")
+logger.debug(
+    f"Thermistor Vcc: {therm.Vcc}, R_fixed: {therm.R_fixed}, max_adc_value: {therm.max_adc_value}"
+)
 
 header = {
     "times": [],
@@ -27,20 +37,28 @@ header = {
 }
 
 cnt = 0
+logging.info("Starting temperature readings")
 while True:
     try:
         temp = therm.read_temperature()
     except ValueError as e:
-        print(f"Error reading temperature: {e}")
+        err_msg = f"Error reading temperature: {e}"
+        logger.error(err_msg)
         temp = None
     if temp is None:
+        logger.debug("No temperature reading, waiting.")
         time.sleep(SLEEP_TIME)
         continue
     all_readings[cnt] = temp
     current_time = time.time()
     header["times"].append(current_time)
+    logger.debug(
+        f"Reading {cnt}: {temp} °C at {datetime.fromtimestamp(current_time)}"
+    )
     if cnt == N_READINGS - 1:
         filename = f"temp_{datetime.now().strftime('%Y%m%d_%H%M%S')}.npz"
         np.savez(filename, data=all_readings, header=header)
-        print(f"Saved {filename}")
+        logger.info(f"Saved {filename}")
         cnt = 0
+    cnt += 1
+    time.sleep(SLEEP_TIME)
