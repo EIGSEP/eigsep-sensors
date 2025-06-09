c6a3c62d5502832b05c294fa15e22b23254162f1
pronav5000 <pranavkarthik@berkeley.edu>
2025-06-09 13:52:58 -0700

    pulls functions correctly now

--- src/eigsep_sensors/imu/__init__.py
+++ src/eigsep_sensors/imu/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "EIGSEP Team"
 __version__ = "0.0.1"
 
-from . import imu_mma8451_orientation
+from .imu_mma8451_orientation import calculate_orientation, get_orientation_unit_vector, get_pitch_roll_from_unit_vector, angle_with_vertical
 from .imu import IMU_MMA8451, IMU_BNO085
